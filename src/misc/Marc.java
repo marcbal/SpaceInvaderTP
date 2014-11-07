80d10ed8a339f6f09a4c4cb4ec82cfb0b7113234
Kaisow <kaisow@live.fr>
2014-11-07 20:40:54 +0100

    Petites modifications
    Tout est dans le titre

--- src/misc/Marc.java
+++ src/misc/Marc.java
@@ -2,6 +2,10 @@ package misc;
 
 import base.Game;
 
+/**
+ * Class which made appear the terrifying Marc !!
+ *
+ */
 public class Marc extends Level {
 
 	public Marc(Game g, int r, int l, int spLR, int spTB, Position p) {
