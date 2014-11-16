358b6e951122c8cdef011b468cd1234a2d69e163
Marc Baloup <marc.baloup@laposte.net>
2014-11-16 03:16:11 +0100

    Correction du précédent commit

---
+++ src/fr/univ_artois/iut_lens/spaceinvader/entities/EntityShotFromAllyAdvanced.java
@@ -0,0 +1,14 @@
+package fr.univ_artois.iut_lens.spaceinvader.entities;
+
+import fr.univ_artois.iut_lens.spaceinvader.EntitiesManager;
+import fr.univ_artois.iut_lens.spaceinvader.util.Position;
+
+public class EntityShotFromAllyAdvanced extends EntityShotFromAlly {
+
+	public EntityShotFromAllyAdvanced(double x, double y, Position s,
+			EntitiesManager eM) {
+		super("sprites/shot2.gif", x, y, 2, s, eM);
+		
+	}
+
+}
