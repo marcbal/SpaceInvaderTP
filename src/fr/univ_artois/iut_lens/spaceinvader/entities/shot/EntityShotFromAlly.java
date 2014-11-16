0030c92c0fb2cafc2b81fb02af171da00f033a8d
Marc Baloup <marc.baloup@laposte.net>
2014-11-16 15:40:02 +0100

    Correction du bug : les tirs qui se touchaient ne s'annulaient pas

--- src/fr/univ_artois/iut_lens/spaceinvader/entities/shot/EntityShotFromAlly.java
+++ src/fr/univ_artois/iut_lens/spaceinvader/entities/shot/EntityShotFromAlly.java
@@ -45,16 +45,18 @@ public abstract class EntityShotFromAlly  extends EntityShot{
 			if (other.receiveDegat(this))
 				// notify the game that the alien has been killed
 				Game.gameInstance.notifyAlienKilled();
-			
-			//Si deux tirs se touchent
-			System.out.println(other instanceof EntityShotFromEnnemy);
-			if(other instanceof EntityShotFromEnnemy) {
-				entitiesManager.removeEntity(this);
-				entitiesManager.removeEntity(other);
-			}
 				
 				
 			used = true;
+		}
+		
+
+		
+		//Si deux tirs se touchent
+		System.out.println(other instanceof EntityShotFromEnnemy);
+		if(other instanceof EntityShotFromEnnemy) {
+			entitiesManager.removeEntity(this);
+			entitiesManager.removeEntity(other);
 		}
 	}
 }
